0409520826a2f19f9569fb94e2d6382e8d0aa78a
Alex Mant <al.mantione@gmail.com>
2022-03-03 12:17:28 +0100

    modif link json

--- script/controllers/index.js
+++ script/controllers/index.js
@@ -27,7 +27,7 @@ const searchIngredients = document.querySelector("input.search-ingredients");
 const searchAppliances = document.querySelector("input.search-appliances");
 const searchUstensils = document.querySelector("input.search-ustensils");
 //DATA
-const myJson = "./../data/recipes.json";
+const myJson = "../../data/recipes.json";
 
 /*--Charge la page entière avec tous les modules--*/
 const pageLauncher = async() => {
