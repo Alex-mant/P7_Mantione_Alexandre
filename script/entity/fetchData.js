141bf8e04ba918f1cbd4c38ae307bdecc550d051
Alex Mant <al.mantione@gmail.com>
2022-03-03 11:28:18 +0100

    test link Json

--- script/entity/fetchData.js
+++ script/entity/fetchData.js
@@ -1,6 +1,6 @@
 /*--Traitement des données (JSON) et stockage--*/
 export const fetchData = async(stockageArray) => {
-    await fetch("../../data/recipes.json")
+    await fetch("./../../data/recipes.json")
     .then((res) => res.json())
     .then((promise) => {
       stockageArray.push(...promise.recipes);  
