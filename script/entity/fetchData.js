d8d5e140da92741115b95abc3fefd703b04a36a4
Alex Mant <al.mantione@gmail.com>
2022-03-03 11:44:26 +0100

    add headers fetch json

--- script/entity/fetchData.js
+++ script/entity/fetchData.js
@@ -1,6 +1,11 @@
 /*--Traitement des données (JSON) et stockage--*/
 export const fetchData = async(stockageArray, target, json) => {
-    await fetch(json)
+    await fetch(json,{ 
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+      })
     .then((res) => res.json())
     .then((promise) => {
       stockageArray.push(...promise[target]);  
