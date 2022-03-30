43b275d33338476a2d7aac1da47232200c958b4b
Alex Mant <al.mantione@gmail.com>
2022-03-30 13:07:08 +0200

    css bulle serving recipes

--- script/views/recipesDisplay.js
+++ script/views/recipesDisplay.js
@@ -8,8 +8,9 @@ const createRecipeHtml = (recipe) =>
   `
   <article class="recipe dBlock">
     <div class="recipe__img" style='background: url(${recipe.image});'>
-      <i class="fas fa-utensils"> ${recipe.servings}</i>
-    </div>
+      <i class="fas fa-utensils"></i>
+      <span class="serving">${recipe.servings}</span>
+          </div>
     <div class="recipe__container">
       <div class="recipe__header">
         <h2>${recipe.name}</h2>
